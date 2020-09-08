1cff35fc16ebfe4f6d1ff553b8bf97180130d5a7
Juan Sanchez <juan.sanchez@juanleonardosanchez.com>
2020-09-08 12:10:56 -0400

    Update LazyLoader.php

--- LazyLoader.php
+++ LazyLoader.php
@@ -19,17 +19,17 @@ class LazyLoader{
     public static $dirRoot;
  
     public static function autoload($class_name){
-		$file = dirname(__FILE__) . (strlen(self::$dirRoot) > 0 ? self::$dirRoot : "") . '/' . array_pop(explode("\\", $class_name)) . '.php';
+        $file = dirname(__FILE__) . (strlen(self::$dirRoot) > 0 ? self::$dirRoot : "") . '/' . array_pop(explode("\\", $class_name)) . '.php';
         file_exists($file) ? require_once($file) : "";
     }
 	
     public static function SetBaseDirectory($directory_root){
-		self::$dirRoot = substr($directory_root, -1) == "\\" ? substr($directory_root, 0, -1) : "";
-	}
+        self::$dirRoot = substr($directory_root, -1) == "\\" ? substr($directory_root, 0, -1) : "";
+    }
 	
-	public static function Register(){
-		return spl_autoload_register(__NAMESPACE__ .'\LazyLoader::autoload');
-	}
+    public static function Register(){
+        return spl_autoload_register(__NAMESPACE__ .'\LazyLoader::autoload');
+    }
 }
  
 $LazyLoader = new LazyLoader;
