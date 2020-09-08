2f49683c72bfdcdf617af198f4d83dce5fcb371d
Juan Sanchez <juan.sanchez@juanleonardosanchez.com>
2020-09-08 12:10:22 -0400

    Update LazyLoader.php

--- LazyLoader.php
+++ LazyLoader.php
@@ -2,7 +2,7 @@
 
 /**
  * LazyLoader
- * A fast, strict lazy loader (time ~ 0.0001)
+ * A fast, strict lazy loader (this is purely an example not to be used in production)
  * 
  * Class name must match File name.
  * If class has namespace, must be called via namespace.
@@ -10,7 +10,7 @@
  * @author Juan L. Sanchez <juanleonardosanchez.com>
  * @license MIT
  * @version 1.2.0
- * @internal 06.26.2013
+ * @internal Published-06.26.2013
  */
 
 Namespace LazyLoader;
@@ -19,16 +19,12 @@ class LazyLoader{
     public static $dirRoot;
  
     public static function autoload($class_name){
-		$file = dirname(__FILE__) . 
-		        (strlen(self::$dirRoot) > 0 ? self::$dirRoot : "") . 
-		        '/' . array_pop(explode("\\", $class_name)) . '.php';
-				
-		file_exists($file) ? require_once($file) : "";
+		$file = dirname(__FILE__) . (strlen(self::$dirRoot) > 0 ? self::$dirRoot : "") . '/' . array_pop(explode("\\", $class_name)) . '.php';
+        file_exists($file) ? require_once($file) : "";
     }
 	
-	public static function SetBaseDirectory($directory_root){
-		self::$dirRoot = substr($directory_root, -1) == "\\" ? 
-		                 substr($directory_root, 0, -1) : "";
+    public static function SetBaseDirectory($directory_root){
+		self::$dirRoot = substr($directory_root, -1) == "\\" ? substr($directory_root, 0, -1) : "";
 	}
 	
 	public static function Register(){
